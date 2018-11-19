00fa8741b1a878f87748d04005a8aed779b76810
KonH <konh@yandex.ru>
2018-11-19 21:06:30 +0700

    Time view model example;

---
+++ Assets/Scripts/Adapters/IntToTimeStringAdapter.cs
@@ -0,0 +1,12 @@
+﻿using UnityWeld.Binding;
+
+[Adapter(typeof(int), typeof(string))]
+public class IntToStringAdapter : IAdapter
+{
+	public object Convert(object valueIn, AdapterOptions options) {
+		var intValue = (int)valueIn;
+		var mins = intValue / 60;
+		var secs = intValue % 60; 
+		return string.Format("{0:00}:{1:00}", mins, secs);
+	}
+}
---
+++ Assets/Scripts/ViewModels/TimeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityWeld.Binding;
+
+[Binding]
+public class TimeViewModel : MonoBehaviour, INotifyPropertyChanged {
+	
+	[Binding]
+	public int Seconds {
+		get { return _seconds; }
+		set {
+			if ( value != _seconds ) {
+				_seconds = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+
+	int _seconds = -1;
+
+	void Update() {
+		if ( Time.timeSinceLevelLoad > Seconds + 1 ) {
+			Seconds = (int)Time.timeSinceLevelLoad;
+		}
+	}
+
+	public event PropertyChangedEventHandler PropertyChanged;
+
+	void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+}
